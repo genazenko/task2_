23993fc0f2ca297ae622380863e1c2a00679ae86
Developer <developer@Admins-MacBook-Pro-6.local>
2016-11-09 11:36:40 +0300

    reformated AppConfig

--- src/main/java/by/inventain/config/AppConfig.java
+++ src/main/java/by/inventain/config/AppConfig.java
@@ -1,17 +1,9 @@
 package by.inventain.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
